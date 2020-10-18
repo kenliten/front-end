9b497076f41864b3014c4b8a53caf830ce77b2de
Otoniel Reyes Galay <otonielreyesgalay@gmail.com>
2020-10-18 00:59:49 -0400

    Added the update feature

--- angularjs/todo/js/TODOController.js
+++ angularjs/todo/js/TODOController.js
@@ -7,6 +7,7 @@
         $scope.todos = [];
         $scope.todoText = "";
         $scope.actualTodoText = "";
+        $scope.actualTodoId = 0;
         $scope.saveTodo = function(){
             let id = $scope.todos.length;
 
@@ -20,15 +21,38 @@
 
             $scope.todoText = "";
         }
-        $scope.evalKey = function(ev){
+        $scope.evalKey = function(ev, foo = "save"){
             if (ev.key == "Enter"){
-                $scope.saveTodo();
+                if (foo == "save"){
+                    $scope.saveTodo();
+                }else if (foo == "edit"){
+                    $scope.updateTodo();
+                }
             }
         }
-        $scope.updateTodo = function(){}
+        $scope.updateTodo = function(){
+            $scope.todos.forEach(function(td){
+                if (td.id == $scope.actualTodoId){
+                    td.text = $scope.actualTodoText;
+                }
+            });
+            $scope.closeEditor();
+        }
         $scope.deleteTodo = function(t) {
             $scope.todos = $scope.todos.filter(x => x.id != t);
             $scope.todos.forEach((td, i) => td.id = i);
+        }
+        $scope.openTodoEditor = function(id) {
+            document.querySelector("#todo-editor-container").style.display = "block";
+            $scope.actualTodoId = id;
+            $scope.todos.forEach(td => {
+                if (td.id == id){
+                    $scope.actualTodoText = td.text;
+                }
+            });
+        }
+        $scope.closeEditor = function(){
+            document.querySelector("#todo-editor-container").style.display = "none";
         }
     })
 
